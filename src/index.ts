051a341086616c9282f1070291fb15ccfde218fd
Syed Asad Ali Rizvi <aley.rizvi@icloud.com>
2020-01-19 05:03:52 +0200

    ADDED: webpack, ts-node, nodemon

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,25 @@ const options: ServerOptions = {
 }
 
 const server = new Server(options)
+
+// TODO: Fetch and load routes
+
+
+// Initialise funciton for supertest
+
+export const init = async (): Promise<Server> => {
+    await server.initialize()
+
+    return server
+}
+
+// funciton for starting the server
+
+export const liftOff = async (): Promise<Server> => {
+    await server.start()
+
+    // TODO: replace console with winston
+    console.log(`Server running at: ${server.info.uri}`);
+
+    return server
+}
