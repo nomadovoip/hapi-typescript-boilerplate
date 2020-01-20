a52b32cdfefd0c294d7e33d6db141d4d70b36e18
Syed Asad Ali Rizvi <aley.rizvi@icloud.com>
2020-01-20 11:40:14 +0200

    Event configure

--- src/eventListeners/console.event.ts
+++ src/eventListeners/console.event.ts
@@ -1,10 +1,21 @@
 import Podium from '@hapi/podium'
 import {iplugin} from '../config/interfaces'
 
+
 const plugin: iplugin =  {
     name: 'event listener',
-    register: function (server, options){
-        console.log("inside event")
+    register: async function (server, options){
+        const emitter: Podium = new Podium()
+        
+        emitter.registerEvent({
+            name: 'index.route',
+            channels: ['log']
+        })
+
+        server.app['emitter'] = emitter
+
+        emitter.on("index.route", async (payload) => console.log(payload))
+
     }
 }
 
